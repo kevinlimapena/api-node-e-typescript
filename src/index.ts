e852c9f53875dbd3bb6aa2cedba2983630899dec
Kevin lima pena <kevinlimapena@gmail.com>
2026-01-02 17:07:49 -0400

    feat: add server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
+import { Knex } from "./servidor/database/knex";
 import { server } from "./servidor/servidores";
 import { con } from "./servidor/shared/envs/conexion"
 // con.connect()
 //   .then(() => console.log("PostgreSQL conectado"))
 //   .catch(err => console.error("Erro ao conectar:", err));
 
+const startServer = () => {
 
-server.listen(process.env.PORT, () => {
-  console.log(`App rodando na porta ${process.env.PORT}`);
-});
+  server.listen(process.env.PORT, () => {
+    console.log(`App rodando na porta ${process.env.PORT}`);
+  });
+
+}
+
+if (process.env.IS_LOCALHOST !== 'true') {
+
+  Knex.migrate.latest()
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
