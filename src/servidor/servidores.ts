dd2acbaa222234ebbea8df01f21cccb87ad794d6
Kevin lima pena <kevinlimapena@gmail.com>
2026-01-08 14:24:44 -0400

    feat : add enable cors

--- src/servidor/servidores.ts
+++ src/servidor/servidores.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import 'dotenv/config'
-
+import cors from 'cors';
 
 import './shared/services/translateYup';
 
 import { router } from './routes/';
 
 const server = express();
+
+server.use(cors(
+  { origin: process.env.ENABLE_CORS?.split(';') || [] }
+));
 
 server.use(express.json());
 server.use(router);
